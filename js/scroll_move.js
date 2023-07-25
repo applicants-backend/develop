0aa26b0c41750e6a9ce3d2274ec69fe4b8e568d6
raddadda <cyc123m@gmail.com>
2023-07-25 16:01:14 +0900

    yc_mainsection

--- js/scroll_move.js
+++ js/scroll_move.js
@@ -1,7 +1,15 @@
+// 바로가기 버튼
 const button1 = document.getElementById('about-button');
+// 이동할 대상
 const section1 = document.getElementById('section2-container');
 
 button1.addEventListener('click',() =>{
-    window.scrollBy({top:section1.getBoundingClientRect().top, behavior:"smooth"})
-    console.log(11111);
+    window.scrollBy({top:section1.getBoundingClientRect().top-50, behavior:"smooth"})
+})
+
+
+const button2 = document.getElementById('category-button');
+const section2 = document.getElementById('section1-container');
+button2.addEventListener('click',() =>{
+    window.scrollBy({top:section2.getBoundingClientRect().top-50, behavior:"smooth"})
 })
