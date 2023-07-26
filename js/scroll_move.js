607d7c794283e861bbf708c68cb731f85ed4e2c7
raddadda <102424773+raddadda@users.noreply.github.com>
2023-07-26 14:53:13 +0900

    Update scroll_move.js

--- js/scroll_move.js
+++ js/scroll_move.js
@@ -1,12 +1,15 @@
+// 바로가기 버튼
 const button1 = document.getElementById('about-button');
-const section1 = document.getElementById('section1');
+// 이동할 대상
+const section1 = document.getElementById('section2-container');
 
 button1.addEventListener('click',() =>{
-    window.scrollBy({top:section1.getBoundingClientRect().top, behavior:"smooth"})
-    console.log(11111);
+    window.scrollBy({top:section1.getBoundingClientRect().top-50, behavior:"smooth"})
 })
 
-// AOS.init({
-//     easing: 'ease-out-back',
-//     duration: 1000
-// });
+
+const button2 = document.getElementById('category-button');
+const section2 = document.getElementById('section1-container');
+button2.addEventListener('click',() =>{
+    window.scrollBy({top:section2.getBoundingClientRect().top-50, behavior:"smooth"})
+})
